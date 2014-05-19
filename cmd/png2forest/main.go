c81eee17e9008fda99dc51ffa1fd7283999707f3
Karol Marcjan <karol.marcjan@gmail.com>
2014-05-19 16:03:10 +0200

    Finish first version of png2forest

--- cmd/png2forest/main.go
+++ cmd/png2forest/main.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"image"
 	_ "image/png"
+	"math/rand"
 	"os"
+	"time"
 )
 
 func Size(bds image.Rectangle) (width, height int) {
@@ -12,7 +14,11 @@ func Size(bds image.Rectangle) (width, height int) {
 	return bds.Max.X - bds.Min.X, bds.Max.Y - bds.Min.Y
 }
 
+const FIRE_P = 0.1
+
 func main() {
+
+	rand.Seed(time.Now().Unix())
 
 	imgFile, err := os.Open("forest.png")
 	if err != nil {
@@ -33,4 +39,29 @@ func main() {
 	w, h := Size(bds)
 
 	fmt.Printf("%d %d\n", w, h)
+
+	for y := bds.Min.Y; y < bds.Max.Y; y++ {
+		for x := bds.Min.X; x < bds.Max.X; x++ {
+
+			color := img.At(x, y)
+
+			red, _, _, _ := color.RGBA()
+
+			if red == 0 {
+				fmt.Print("S")
+			} else {
+				if rand.Float64() < FIRE_P {
+					fmt.Print("F")
+				} else {
+					fmt.Print("T")
+				}
+			}
+
+			if x+1 == bds.Max.X {
+				fmt.Println()
+			} else {
+				fmt.Print(" ")
+			}
+		}
+	}
 }
