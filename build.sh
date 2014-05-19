d3a9d966fb38c5686326be78ca9932dd658991f2
Karol Marcjan <karol.marcjan@gmail.com>
2014-05-19 23:16:44 +0200

    Improve the build script

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
+go fmt github.com/szabba/ffire/forest
+go fmt github.com/szabba/ffire/cmd/png2forest
+go fmt github.com/szabba/ffire/cmd/ffire
+go fmt github.com/szabba/ffire/cmd/forest2png
 go build github.com/szabba/ffire/cmd/png2forest
 go build github.com/szabba/ffire/cmd/ffire
 go build github.com/szabba/ffire/cmd/forest2png
